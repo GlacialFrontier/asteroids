abda96d47f15c95ad54bd2c9069a2da8332204b5
GlacialFrontier <jadewsobers@gmail.com>
2026-06-15 18:13:18 -0400

    new files added: circleshape & plyer. Spaceship is drawn, and can rotate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import *
+
 
 def main():
     # initialize pygame
@@ -13,11 +15,21 @@ def main():
     # draw the new GUI
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    # player info
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
     # game loop
     while True:
 
         # set logs
         log_state()
+
+        # make quit button active
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
         # event queue start
         for event in pygame.event.get():
@@ -26,16 +38,19 @@ def main():
         # set screen to black
         screen.fill("black")
 
+        # check rotation
+        player.update(dt)
+
+        # draw player
+        player.draw(screen)
+
         # refresh screen
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000  
         
         
-        # make quit button active
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
+        
             
 if __name__ == "__main__":
     main()
