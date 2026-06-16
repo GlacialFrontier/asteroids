dbc59c705c9a77da60e03608dba1ceb3439ad33c
GlacialFrontier <jadewsobers@gmail.com>
2026-06-16 19:45:46 -0400

    add moving functionalitiy to spaceship

--- player.py
+++ player.py
@@ -20,13 +20,35 @@ class Player(CircleShape):
     def draw(self, screen: object):
         pygame.draw.polygon(screen, "White", self.triangle(), width = LINE_WIDTH)
 
+    # rotates player
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
+
+    # move player
+    def move (self, dt):
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
+
+
 
     def update(self, dt: float) -> None:
         keys = pygame.key.get_pressed()
 
+        # rotate counter-clockwise if 'a' is pressed
         if keys[pygame.K_a]:
             self.rotate(-dt)
+
+        # rotate clockwise if 'd' is pressed
         if keys[pygame.K_d]:
             self.rotate(dt)
+
+        # move forwards if 'w' is pressed
+        if keys[pygame.K_w]:
+            self.move(dt)
+
+        # move backwards if 's' is pressed
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
